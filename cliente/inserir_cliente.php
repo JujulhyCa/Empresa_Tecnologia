b5e873735c1c5cb001bd9ca4a1fd88b0bfb8b024
JujulhyCa <julhycarvalho85@gmail.com>
2024-05-25 10:40:40 -0300

    Criar inserir_cliente.php
    Criar inserir_cliente.php

---
+++ cliente/inserir_cliente.php
@@ -0,0 +1,52 @@
+<?php
+    require_once("../cabecalho.php");
+?>
+
+    <h3>Inserir Cliente</h3>
+    <form action="" method="POST">
+        <div class="row">
+            <div class="col">
+                <label for="nome" class="form-label">Informe o nome</label>
+                <input type="text" class="form-control"     name="nome">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <label for="descricao" class="form-label">Informe o telefone</label>
+                <input type="text" class="form-control"     name="descricao">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <label for="valor" class="form-label">Informe o e-mail</label>
+                <input type="text" class="form-control"     name="valor">
+            </div>
+        </div>
+       
+        <div class="row">
+            <div class="col">
+                <button type="submit" class="btn btn-success">
+                    Salvar
+                </button>
+            </div>
+        </div>
+    </form>
+
+<?php
+    // processamento
+    if($_POST){
+        $nome = $_POST['nome'];
+        $telefone = $_POST['telefone'];
+        $email = $_POST['email'];
+        if ($nome!= "" && $telefone !="" && $email != ""){
+            if (inserirProduto($nome,$telefone,$email)){
+                echo "Cliente inserido com sucesso.";
+            } else {
+                echo "Erro ao inserir o cliente.";
+            }
+
+        } else {
+            echo "Preencha todos os campos!";
+        }
+    }
+    require_once("../rodape.php");
