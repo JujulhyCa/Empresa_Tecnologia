7a60b9b997d1028f20b51ae77212eac7e61f09c2
JujulhyCa <julhycarvalho85@gmail.com>
2024-05-25 11:00:05 -0300

    Adequções alterar/ excluir Cliente
    Adequções alterar/ excluir Cliente

---
+++ cliente/alterar_cliente.php
@@ -0,0 +1,36 @@
+<?php
+    require_once("../cabecalho.html");
+?>
+
+    <h3>Alterar Cliente</h3>
+    <form>
+        <div class="row">
+            <div class="col">
+                <label for="nome" class="form-label">Informe o nome</label>
+                <input type="text" class="form-control"     name="nome">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <label for="descricao" class="form-label">Informe o telefone</label>
+                <input type="text" class="form-control"     name="telefone">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <label for="valor" class="form-label">Informe o e-mail</label>
+                <input type="text" class="form-control"     name="email">
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col">
+                <button type="submit" class="btn btn-success">
+                    Salvar
+                </button>
+            </div>
+        </div>
+    </form>
+
+<?php
+    require_once("../rodape.html");
---
+++ cliente/excluir_cliente.php
@@ -0,0 +1,37 @@
+<?php
+    require_once("../cabecalho.html");
+?>
+
+    <h3>Excluir Cliente</h3>
+    <form>
+        <div class="row">
+            <div class="col">
+                <label for="nome" class="form-label">Informe o nome</label>
+                <input type="text" class="form-control"     name="nome" disabled>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <label for="descricao" class="form-label">Informe o telefone</label>
+                <input type="text" class="form-control"     name="telefone" disabled>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <label for="valor" class="form-label">Informe o e-mail</label>
+                <input type="text" class="form-control"     name="email" disabled>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col">
+                <p>Deseja realmente excluir?</p>
+                <button type="submit" class="btn btn-danger">
+                    Excluir
+                </button>
+            </div>
+        </div>
+    </form>
+
+<?php
+    require_once("../rodape.html");
